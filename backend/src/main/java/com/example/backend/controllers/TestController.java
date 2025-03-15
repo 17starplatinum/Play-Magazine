d94f28671049e309d50600b05171d584a50e164d
nkolomiika <kolomiec_ns_0919@1511.ru>
2025-03-15 03:10:59 +0300

    add auth

---
+++ backend/src/main/java/com/example/backend/controllers/TestController.java
@@ -0,0 +1,26 @@
+package com.example.backend.controllers;
+
+import com.example.backend.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+    private final UserService userService;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handler(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
---
+++ backend/src/main/java/com/example/backend/security/jwt/JwtConfig.java
@@ -0,0 +1,15 @@
+package com.example.backend.security.jwt;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class JwtConfig {
+    @Value("${token.signing.key}")
+    private String jwtSigningKey;
+
+    @Value("${token.expire}")
+    private int jwtExpirationMs;
+}
