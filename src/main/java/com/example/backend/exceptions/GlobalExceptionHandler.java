c328d997cafd6e6d237d5c87e775b11cde422922
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2025-04-26 17:39:46 +0300

    fix: :bug: EVEN MORE exception handling

--- src/main/java/com/example/backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/backend/exceptions/GlobalExceptionHandler.java
@@ -12,12 +12,14 @@ import com.example.backend.exceptions.notfound.CardNotFoundException;
 import com.example.backend.exceptions.paymentrequired.AppNotPurchasedException;
 import com.example.backend.exceptions.prerequisites.*;
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.WeakKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -32,10 +34,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({
             BadCredentialsException.class
     })
-    public ResponseEntity<Object> handleBadCredentialsException(Exception e) {
+    public ResponseEntity<Object> handleBadCredentialsException() {
         ErrorResponseDto responseDto = new ErrorResponseDto(
                 HttpStatus.UNAUTHORIZED.value(),
-                e.getMessage(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(responseDto, HttpStatus.UNAUTHORIZED);
@@ -44,12 +46,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({
             InvalidRequestException.class,
             JwtException.class,
-            WeakKeyException.class
+            WeakKeyException.class,
+            MalformedJwtException.class,
+            HttpMediaTypeNotSupportedException.class
     })
-    public ResponseEntity<Object> handleBadRequestException(Exception e) {
+    public ResponseEntity<Object> handleBadRequestException() {
         ErrorResponseDto responseDto = new ErrorResponseDto(
                 HttpStatus.BAD_REQUEST.value(),
-                e.getMessage(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
@@ -60,10 +64,10 @@ public class GlobalExceptionHandler {
             CardNotFoundException.class,
             NoResourceFoundException.class
     })
-    public ResponseEntity<Object> handleUserNotFoundException(Exception e) {
+    public ResponseEntity<Object> handleUserNotFoundException() {
         ErrorResponseDto responseDto = new ErrorResponseDto(
                 HttpStatus.NOT_FOUND.value(),
-                e.getMessage(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(responseDto, HttpStatus.NOT_FOUND);
@@ -80,10 +84,10 @@ public class GlobalExceptionHandler {
             ReviewAlreadyExistsException.class,
             HttpRequestMethodNotSupportedException.class
     })
-    public ResponseEntity<Object> handleBadPrerequisiteException(Exception e) {
+    public ResponseEntity<Object> handleBadPrerequisiteException() {
         ErrorResponseDto responseDto = new ErrorResponseDto(
                 HttpStatus.PRECONDITION_FAILED.value(),
-                e.getMessage(),
+                HttpStatus.PRECONDITION_FAILED.getReasonPhrase(),
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(responseDto, HttpStatus.PRECONDITION_FAILED);
@@ -95,40 +99,40 @@ public class GlobalExceptionHandler {
             RequestPendingException.class,
             EmailSendingException.class
     })
-    public ResponseEntity<Object> handleIncompleteProcessingException(Exception e) {
+    public ResponseEntity<Object> handleIncompleteProcessingException() {
         ErrorResponseDto responseDto = new ErrorResponseDto(
-            HttpStatus.ACCEPTED.value(),
-                e.getMessage(),
+                HttpStatus.ACCEPTED.value(),
+                HttpStatus.ACCEPTED.getReasonPhrase(),
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(responseDto, HttpStatus.ACCEPTED);
     }
 
     @ExceptionHandler(AppNotPurchasedException.class)
-    public ResponseEntity<Object> handleAppNotPurchasedException(Exception e) {
+    public ResponseEntity<Object> handleAppNotPurchasedException() {
         ErrorResponseDto responseDto = new ErrorResponseDto(
                 HttpStatus.PAYMENT_REQUIRED.value(),
-                e.getMessage(),
+                HttpStatus.PAYMENT_REQUIRED.getReasonPhrase(),
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(responseDto, HttpStatus.PAYMENT_REQUIRED);
     }
 
     @ExceptionHandler(AlreadyInRoleException.class)
-    public ResponseEntity<Object> handleAlreadyInRoleException(Exception e) {
+    public ResponseEntity<Object> handleAlreadyInRoleException() {
         ErrorResponseDto responseDto = new ErrorResponseDto(
                 HttpStatus.CONFLICT.value(),
-                e.getMessage(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(responseDto, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<Object> handleAccessDeniedException(Exception e) {
+    public ResponseEntity<Object> handleAccessDeniedException() {
         ErrorResponseDto responseDto = new ErrorResponseDto(
                 HttpStatus.FORBIDDEN.value(),
-                e.getMessage(),
+                HttpStatus.FORBIDDEN.getReasonPhrase(),
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(responseDto, HttpStatus.FORBIDDEN);
